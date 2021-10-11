291bbe12c066f0cb25d3658e6710fd1efd6fc39d
ITITIU20264 <ITITIU20264@student.hcmiu.edu.vn>
2021-10-11 17:02:20 +0700

    Exercise for week 2 (Exercise 1 2 3 4)

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -15,7 +15,23 @@ int main(int argc, char *argv[]) {
 	int testcase = atoi(argv[1]);
 	
 	//Your codes here
-	
-	
+	int arr[22] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 33, 37, 41, 43, 47, 49, 53, 57, 59, 61, 67, 71};
+	int count = 0;
+	while((testcase != 1) && (count <= 19))
+	{
+		while(testcase % arr[count] == 0)
+		{
+			if(testcase / arr[count] != 1)
+			{
+				printf("%d * ", arr[count]);
+			}	
+			else
+			{
+				printf("%d", arr[count]);
+			}
+			testcase /= arr[count];
+		}
+		count++;
+	}
 	return 0;
 }
